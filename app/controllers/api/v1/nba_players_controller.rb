7bb4ec7bf93e19233b70455e691ac58023426a54
orenmagid <autoimpedicis@gmail.com>
2018-08-18 17:12:11 -0400

    push to Heroku

--- app/controllers/api/v1/nba_players_controller.rb
+++ app/controllers/api/v1/nba_players_controller.rb
@@ -3,19 +3,19 @@ class Api::V1::NbaPlayersController < ApplicationController
 before_action :find_player, only: [:show, :update]
 
  def index
-   # @nba_players_top = NbaPlayer.all.select { |player| player.pie >= "0.15" && player.person_id != 201951 && player.person_id != 202688 && player.person_id != 201582}.sample(2)
-   # @nba_players_mid = NbaPlayer.all.select { |player| player.pie > "0.10" && player.pie < "0.15" && player.person_id != 201951 && player.person_id != 202688 && player.person_id != 201951}.sample(2)
-   # @nba_players_rest = NbaPlayer.all.select { |player| player.pie <= "0.1" && player.person_id != 201951 && player.person_id != 202688 && player.person_id != 201582}.sample(1)
+   @nba_players_top = NbaPlayer.all.select { |player| player.pie >= "0.15" && player.person_id != 201951 && player.person_id != 202688 && player.person_id != 201582}.sample(2)
+   @nba_players_mid = NbaPlayer.all.select { |player| player.pie > "0.10" && player.pie < "0.15" && player.person_id != 201951 && player.person_id != 202688 && player.person_id != 201951}.sample(2)
+   @nba_players_rest = NbaPlayer.all.select { |player| player.pie <= "0.1" && player.person_id != 201951 && player.person_id != 202688 && player.person_id != 201582}.sample(1)
 
 
 
-   # @nba_players = @nba_players_top + @nba_players_mid + @nba_players_rest
+   @nba_players = @nba_players_top + @nba_players_mid + @nba_players_rest
 
 
 
 
 
-   @nba_players = NbaPlayer.all.select { |player| player.pie == "undefined" && player.person_id != 201951 && player.person_id != 202688 && player.person_id != 201582}.sample(12)
+   # @nba_players = NbaPlayer.all.select { |player| player.pie == "undefined" && player.person_id != 201951 && player.person_id != 202688 && player.person_id != 201582}.sample(12)
    render json: @nba_players
  end
 
