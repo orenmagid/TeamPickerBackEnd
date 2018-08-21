09f79c6cc506c50d16194321fd4c876a16e32124
orenmagid <autoimpedicis@gmail.com>
2018-08-21 10:06:44 -0400

    Group Controller

--- app/controllers/api/v1/user_groups_controller.rb
+++ app/controllers/api/v1/user_groups_controller.rb
@@ -6,7 +6,7 @@ class Api::V1::UserGroupsController < ApplicationController
     debugger
     @user_group = UserGroup.find(params[:id])
     @user_group.destroy
-    render json: status: :success
+    
 
   end
 
