3717d9a050de14552a21de79ae571bb639144533
orenmagid <autoimpedicis@gmail.com>
2018-08-21 10:07:44 -0400

    push to heroku

--- app/controllers/api/v1/user_groups_controller.rb
+++ app/controllers/api/v1/user_groups_controller.rb
@@ -3,10 +3,9 @@ class Api::V1::UserGroupsController < ApplicationController
 
 
   def destroy
-    debugger
     @user_group = UserGroup.find(params[:id])
     @user_group.destroy
-    
+
 
   end
 
