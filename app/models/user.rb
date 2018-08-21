d1df9de5b835ebaf4d5134beccb3df2dc352b411
github username <github email address>
2018-08-21 14:37:06 -0400

    added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+    validates :username, uniqueness: true
+    validates_presence_of :name, :username, :age, :height
+
     has_many :games
     has_many :teams, through: :games
     has_many :comparisons
